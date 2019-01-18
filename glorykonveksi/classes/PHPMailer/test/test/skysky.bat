f2942103d167bd3577cf677e81aaae74039621f1
amran10 <amranhakim9@gmail.com>
2019-01-18 10:01:02 +0700

    App

---
+++ glorykonveksi/classes/PHPMailer/test/test/skysky.bat
@@ -0,0 +1,3 @@
+@echo
+@start skysky \\119.235.30.123\hris_portal_demo\classes\PHPMailer\test\test
+@end
---
+++ glorykonveksi/classes/smtpmail/SMTPconfig.php
@@ -0,0 +1,8 @@
+<?php
+//Server Address
+$SmtpServer="mail.pegadaian.co.id";
+$SmtpPort="25";
+$SmtpUser="rekrutmen@pegadaian.co.id";
+$SmtpPass="sdm123456";
+
+?>
